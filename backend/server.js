3b72e7bb5f0156d6c0fe12f8066db6251f93841c
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-07-31 20:10:23 -0300

    :books: att

--- backend/server.js
+++ backend/server.js
@@ -32,19 +32,17 @@ app.set('view engine', 'ejs');
 
 app.set(
     'views',
-    path.join(__dirname, 'src', 'views')
+    path.join(__dirname, '..', 'frontend', 'views')
 );
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
     express.static(
-        path.join(__dirname, 'public')
+        path.join(__dirname, '..', 'frontend', 'public')
     )
 );
-
 
 // páginas
 
